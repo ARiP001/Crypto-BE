7cb0f15952a9527e6ebd5c1289e87d38b1b84905
FadhilD <mefx226@gmail.com>
2025-05-25 12:30:35 +0700

    5173

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000',
+  origin: ['http://localhost:5173',
     //BUAT FRONTEND NANTI MASUKIN SINI
   ],
   credentials: true
